9f177dfd91dbc565a6606343cc784bb55383d28f
stavby <stav134679@gmail.com>
2024-11-22 17:09:22 +0200

    add mongodb support

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import { startDB } from './services/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || '8080';
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+startDB();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!`)
-})
+    console.log(`Server is running on port ${PORT}!`);
+});
---
+++ services/db.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export const startDB = () => {
+    const { DB_URL } = process.env;
+    if (!DB_URL) {
+        throw new Error('DB_URL env variable missing');
+    }
+
+    mongoose.connect(DB_URL);
+    const db = mongoose.connection;
+    db.on('error', (error) => console.error(error));
+    db.once('open', () => console.log('Connected to mongo database'));
+};
