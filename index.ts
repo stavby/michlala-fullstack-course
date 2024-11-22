48bfc903320fc46c5972baa6798477d3132b9daa
stavby <stav134679@gmail.com>
2024-11-22 17:26:57 +0200

    add /isAlive

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import { startDB } from './services/db';
 import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 
 dotenv.config();
 
@@ -12,6 +14,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 startDB();
+
+app.get('/isAlive', (req: Request, res: Response) => {
+    res.status(httpStatus.OK).send('Server is alive!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
