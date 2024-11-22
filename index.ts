4b7a82c60b48c844cdadb9365cfd6b4f16ec75bd
stavby <stav134679@gmail.com>
2024-11-22 16:42:18 +0200

    created base server

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const PORT = process.env.PORT || '8080';
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`)
+})
