5e029bc2cbd4656903c7f8f7bd5aa66892eacaaf
Yaron Dlugin <yaron206@gmail.com>
2024-12-28 17:34:25 +0200

    feat(users): add swagger documentation

--- models/users.ts
+++ models/users.ts
@@ -25,3 +25,45 @@ export const userModel = model('users', userSchema);
 export type User = TypeWithId<InferRawDocType<typeof userSchemaDefinition>>;
 
 export type UserDetails = Pick<User, 'password' | 'email'>;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - username
+ *         - email
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: The username of the user. Is unique.
+ *         email:
+ *           type: string
+ *           description: The email of the user. Is unique.
+ *       example:
+ *         username: "shlomi"
+ *         email: "shlomi@gmail.com"
+ *     NewUser:
+ *       type: object
+ *       required:
+ *         - username
+ *         - email
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: The username of the user. Is unique.
+ *         email:
+ *           type: string
+ *           description: The email of the user. Is unique.
+ *         password:
+ *           type: string
+ *           description: The password of the user.
+ *       example:
+ *         username: "shlomi"
+ *         email: "shlomi@gmail.com"
+ *         password: "shlomispassword123"
+ */
--- routes/users.ts
+++ routes/users.ts
@@ -8,3 +8,95 @@ usersRouter.get('/', getUserByDetails);
 usersRouter.put('/:id', updateUserById);
 usersRouter.delete('/:id', deleteUserById);
 usersRouter.use(createErrorHandler('users'));
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get a user by details
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: username
+ *         in: query
+ *         schema:
+ *           type: string
+ *         description: Find user by username
+ *       - name: email
+ *         in: query
+ *         schema:
+ *           type: string
+ *         description: Find user by email
+ *     responses:
+ *       200:
+ *         description: A user's details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad Request (Invalid query parameters)
+ * /users/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to get
+ *     responses:
+ *       200:
+ *         description: A user's details
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad Request (Invalid ID)
+ *   put:
+ *     summary: Update a user by ID
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NewUser'
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad Request (Invalid ID or missing fields)
+ *   delete:
+ *     summary: Delete a user by ID
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to delete
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       400:
+ *         description: Bad Request (Invalid ID)
+ */
